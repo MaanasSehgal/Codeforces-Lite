00263df084155d316748b26c7b02962b2a8d4654
Devendra Suryavanshi <devendrasooryavanshee@gmail.com>
2025-02-21 14:08:48 +0530

    now handle float time limits

--- src/utils/dom/getTimeLimit.ts
+++ src/utils/dom/getTimeLimit.ts
@@ -6,8 +6,8 @@ export const getTimeLimit = async () => {
             const timeLimitElement = document.querySelector('.time-limit');
             if (!timeLimitElement) return 0;
             const timeText = timeLimitElement.textContent || '';
-            const matches = timeText.match(/(\d+)/);
-            return matches ? parseInt(matches[0]) : 0;
+            const matches = timeText.match(/(\d+\.?\d*)/);
+            return matches ? parseFloat(matches[0]) : 0;
         }
     });
 
